18314f9ccfa9f645d2066145a1da6cd3de6fdb70
sainikmandal <68194201+sainik-create@users.noreply.github.com>
2025-01-19 18:32:31 +0530

    Scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import Link from "next/link";
 
+const mockURL = [
+  "https://9ikcqz2gxn.ufs.sh/f/huxzmsdSjr2J5TB3IMV89cA6bJWKnC5BRItp2v3qgQakdeE1",
+  "https://9ikcqz2gxn.ufs.sh/f/huxzmsdSjr2J0ov0sBiBKh3AYjft4aRvMZ7eE2FNXmHpPciI",
+  "https://9ikcqz2gxn.ufs.sh/f/huxzmsdSjr2JOT5mS82SNWamXF6flng0ezMVpHwDtYUEb7y9",
+  "https://9ikcqz2gxn.ufs.sh/f/huxzmsdSjr2Jez5rqftZk6lUqNOne1MpPrVi2B8H7zQbsWTS",
+];
+
+const mockImages = mockURL.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress){" "}
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image, idx) => (
+          <div key={`${image.id}-${idx}`} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
+      <p>Hello (gallery in progress)</p>
     </main>
   );
 }
